fbc8788c7d3b85324a4424d429a609b78d987ac1
Geovani Miguel <geovanimiguelamg@gmail.com>
2025-11-10 15:51:26 +0100

    DOM + Condições

---
+++ ex08/index.js
@@ -0,0 +1,6 @@
+var vel = 85
+console.log(`A velocidade do seu carro é de ${vel}Km/H`)
+if (vel > 60) { // condição simples: pk não tem senão Else
+    console.log(`Você ultrapassou o limte de velocidade permitida. MUTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
