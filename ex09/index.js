e3bd3d45644c15f568d44b2c9b343af3577a11ac
Geovani Miguel <geovanimiguelamg@gmail.com>
2025-11-14 14:39:57 +0100

    Dor de cabeça

--- ex09/index.js
+++ ex09/index.js
@@ -1,8 +1,31 @@
-var pais = "Estados Unidos"
+// Exercício 09 - Condicional Simples e Composta
+
+// Condição Simples
+/* var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+    if (vel > 60) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+    }
+console.log(`Dirija sempre usando cinto de segurança!`); */
+
+
+// Condição Composta
+
+
+/* var valor = 8.5
+  console.log(`O valor da sua compra é ${valor}`);
+    if (valor >= 8) {
+        console.log("Apto");
+    }
+    else {
+  console.log(`Não Apto`);
+    } */
+
+ var pais = "Angola"
 console.log(`Vivendo em ${pais}`);
-if (pais == "Brasil") {
-    console.log("Você é Brasileiro!");
+if (pais != "Angola") {
+    console.log("Você é angolano!");
 }
 else {
     console.log("Você é Estrangeiro!");
-}
+} 
