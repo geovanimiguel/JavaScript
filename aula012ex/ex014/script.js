dd5d80cc09738da5870213605f7c47bff8be5f25
Geovani Miguel <geovanimiguelamg@gmail.com>
2025-11-27 16:36:20 +0100

    desafio da hora

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,21 @@
-window.alert("Bem-vindo ao meu site!");
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+        if (hora >= 5 && hora < 12) {
+            //Bom dia!
+            foto.src = 'img/manha.jpg'
+    } else if (hora >= 12 && hora < 18) {
+            //Boa tarde!
+            foto.src = 'img/tarde.jpg'
+    } else if (hora >= 0 && hora <= 4) {
+            //Boa madrugada!
+            foto.src = 'img/madrugada.jpg'
+        } else {
+            //Boa noite!
+            foto.src = 'img/noite.jpg'
+    }
+}
