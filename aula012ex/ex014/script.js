6e2bd4e1390c2988e41e47ebed96d642773301e1
Geovani Miguel <geovanimiguelamg@gmail.com>
2025-11-28 20:59:33 +0100

    Mudança de cor

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -11,11 +11,14 @@ function carregar() {
     } else if (hora >= 12 && hora < 18) {
             //Boa tarde!
             foto.src = 'img/tarde.jpg'
+            document.body.style.background = '#b9846f'
     } else if (hora >= 0 && hora <= 4) {
             //Boa madrugada!
             foto.src = 'img/madrugada.jpg'
+            document.body.style.background = '#515154'
         } else {
             //Boa noite!
             foto.src = 'img/noite.jpg'
+            document.body.style.background = '#0b1e3f'
     }
 }
